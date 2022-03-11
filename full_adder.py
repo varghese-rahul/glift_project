ad4e143f9433e9d2ba632c5207769e7de88bebd2
PranjaliJain <pranjali.jain007@gmail.com>
2022-03-10 18:39:10 -0800

    changes to include ints

--- full_adder.py
+++ full_adder.py
@@ -1,17 +1,54 @@
 import pyrtl
 
-from gates import *
+def and_gate(a, b, a_t, b_t):
+    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
+
+    # regular AND
+    o = a & b
+
+    # glift AND
+    an_t = ~a_t
+    bn_t = ~b_t
+    o_t = a_t & b_t | a & an_t & b_t | b & a_t & bn_t
+
+    return o, o_t
+
+def or_gate(a, b, a_t, b_t):
+    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
+
+    # regular OR
+    o = a | b
+
+    # glift OR
+    an_t = ~a_t
+    bn_t = ~b_t
+    an = ~a
+    bn = ~b
+    o_t = a_t & b_t | an & an_t & b_t | bn & a_t & bn_t
+
+    return o, o_t
+
+def xor_gate(a, b, a_t, b_t):
+    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
+
+    # regular XOR
+    o = a ^ b
+
+    # glift XOR
+    an_t = ~a_t
+    bn_t = ~b_t
+    o_t = a_t & b_t | an_t & b_t | a_t & bn_t
+    return o, o_t
 
 
-def half_adder(a, b, a_t, b_t):
-    sum, sum_t = xor_gate(a, b, a_t, b_t)
-    carry, carry_t = and_gate(a, b, a_t, b_t)
-    return sum, carry, sum_t, carry_t
+def xnor_gate(a, b, a_t, b_t):
+    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
 
-def full_adder(a, b, c, a_t, b_t, c_t):
-    sum1, carry1, sum1_t, carry1_t = half_adder(a, b, a_t, b_t)
-    sum2, carry2, sum2_t, carry2_t = half_adder(c, sum1, c_t, sum1_t)
-    sum, sum_t = sum2, sum2_t
-    carry, carry_t = or_gate(carry1, carry2, carry1_t, carry2_t)
-    return sum, carry, sum_t, carry_t
+    # regular XNOR
+    o = ~(a ^ b)
 
+    # glift XNOR
+    an_t = ~a_t
+    bn_t = ~b_t
+    o_t = ~(a_t & b_t | an_t & b_t | a_t & bn_t)
+    return o, o_t
