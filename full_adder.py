b1d38cfe0585453df551974520b1e110c8c3b3e6
PranjaliJain <pranjali.jain007@gmail.com>
2022-03-10 18:48:20 -0800

    fixed full adder

--- full_adder.py
+++ full_adder.py
@@ -1,54 +1,28 @@
 import pyrtl
 
-def and_gate(a, b, a_t, b_t):
-    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
+from gates import *
+BITMAX = 32
 
-    # regular AND
-    o = a & b
+def half_adder(a, b, a_t, b_t):
+    sum, sum_t = xor_gate(a, b, a_t, b_t)
+    carry, carry_t = and_gate(a, b, a_t, b_t)
+    return sum, carry, sum_t, carry_t
 
-    # glift AND
-    an_t = ~a_t
-    bn_t = ~b_t
-    o_t = a_t & b_t | a & an_t & b_t | b & a_t & bn_t
+def full_adder(a, b, c, a_t, b_t, c_t):
+    sum1, carry1, sum1_t, carry1_t = half_adder(a, b, a_t, b_t)
+    sum2, carry2, sum2_t, carry2_t = half_adder(c, sum1, c_t, sum1_t)
+    sum, sum_t = sum2, sum2_t
+    carry, carry_t = or_gate(carry1, carry2, carry1_t, carry2_t)
+    return sum, carry, sum_t, carry_t
 
-    return o, o_t
-
-def or_gate(a, b, a_t, b_t):
-    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
-
-    # regular OR
-    o = a | b
-
-    # glift OR
-    an_t = ~a_t
-    bn_t = ~b_t
-    an = ~a
-    bn = ~b
-    o_t = a_t & b_t | an & an_t & b_t | bn & a_t & bn_t
-
-    return o, o_t
-
-def xor_gate(a, b, a_t, b_t):
-    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
-
-    # regular XOR
-    o = a ^ b
-
-    # glift XOR
-    an_t = ~a_t
-    bn_t = ~b_t
-    o_t = a_t & b_t | an_t & b_t | a_t & bn_t
-    return o, o_t
-
-
-def xnor_gate(a, b, a_t, b_t):
-    # assert (len(a) == len(b) == len(a_t) == len(b_t) == 1)
-
-    # regular XNOR
-    o = ~(a ^ b)
-
-    # glift XNOR
-    an_t = ~a_t
-    bn_t = ~b_t
-    o_t = ~(a_t & b_t | an_t & b_t | a_t & bn_t)
-    return o, o_t
+def full_adder_32bit(a_32, b_32, c_32, a_t_32, b_t_32, c_t_32):
+    s_32, s_t_32 = 0, 0 
+    
+    c, c_t = c_32 & 1, c_t_32 & 1 
+    for i in range(BITMAX):
+        a, b = a_32>>i & 1, b_32>>i & 1 
+        a_t, b_t = a_t_32>>i & 1, b_t_32>>i & 1
+        s, c, s_t, c_t = full_adder(a, b, c, a_t, b_t, c_t)
+        s_32 += s<<i
+        s_t_32 += s_t<<i
+    return s_32, c, s_t_32, c_t
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import pyrtl
 
 from gates import *
-from gates_32bit import *
 from full_adder import *
 from multiplier import *
 from comparator import *
 
 
-def execute(op, a, b, a_t, b_t, c=None, c_t=None):
+def execute(op, a, b, c=None, a_t=None, b_t=None, c_t=None):
     if op=="AND":
-        o, o_t = and_gate_32bit(a, b, a_t, b_t)
+        o, o_t = and_gate(a, b, a_t, b_t)
     elif op=="OR":
-        o, o_t = or_gate_32bit(a, b, a_t, b_t)
+        o, o_t = or_gate(a, b, a_t, b_t)
     elif op=="XOR":
-        o, o_t = xor_gate_32bit(a, b, a_t, b_t)
+        o, o_t = xor_gate(a, b, a_t, b_t)
     elif op=="XNOR":
-        o, o_t = xnor_gate_32bit(a, b, a_t, b_t)
+        o, o_t = xnor_gate(a, b, a_t, b_t)
     elif op=="ADD":
         sum, carry, sum_t, carry_t = full_adder_32bit(a, b, c, a_t, b_t, c_t)
+        o, o_t = [sum, carry], [sum_t, carry_t]
     elif op=="MULT":
-        p, p_t = multiplier_32bit(a, b, a_t, b_t)
+        o, o_t = multiplier_32bit(a, b, a_t, b_t)
     elif op=="COMP":
         less, equal, greater, less_t, equal_t, greater_t = comparator_32bit(a, b, a_t, b_t)
+        o, o_t = [less, equal, greater], [less_t, equal_t, greater_t]
+
+    return o, o_t
 
 
 
+    
+if __name__=='__main__':
+    a = input("enter a: ")
+    b = input("enter b: ")
+    c = input("enter c: ")
+    a_t = input("enter a_t: ")
+    b_t = input("enter b_t: ")
+    c_t = input("enter c_t: ")
+    # o,o_t= full_adder_32bit(int(a),int(b),int(c), int(a_t),int(b_t), int(c_t))
+    o,o_t= execute("ADD", int(a),int(b),int(c), int(a_t),int(b_t), int(c_t))
+    print(o,o_t)
